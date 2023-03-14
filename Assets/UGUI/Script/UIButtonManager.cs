dea572dfbd9c680171eae5c61621826c208ddb8a
durnfish <tkgkdgksek12@gmail.com>
2023-03-15 01:05:37 +0900

    Page Select Button change Toggle, Toggle highlight(Complete)

--- Assets/UGUI/Script/UIButtonManager.cs
+++ Assets/UGUI/Script/UIButtonManager.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIButtonManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _selectToggle, _recordToggle;
     [SerializeField] private GameObject _selectArea, _recordArea;
     [SerializeField] private GameObject _closeUi;
-    private bool _isSelect, _isRecord;
+
+    [SerializeField] private Sprite _highlight;
+
+    private Toggle _isSelect, _isRecord;
+    private Color _defaultColor, _highlightColor;
+
 
     private void Awake()
     {
-        _isSelect = true;
-        _isRecord = false;
+        _isSelect = _selectToggle.GetComponent<Toggle>();
+        _isRecord = _recordToggle.GetComponent<Toggle>();
+
+        _defaultColor = _selectToggle.GetComponent<Image>().color;
+        _highlightColor = new Color(0.7f,0.7f,0.7f,1);
+
+        _isSelect.isOn = true;
     }
 
     public void SelectButton()
     {
-        _isSelect = true;
-        _isRecord = false;
+        _selectToggle.GetComponent<Image>().sprite = _highlight;
+        _selectToggle.GetComponent<Image>().color = _highlightColor;
+
+        _recordToggle.GetComponent<Image>().sprite = null;
+        _recordToggle.GetComponent<Image>().color = _defaultColor;
 
         _selectArea.SetActive(true);
         _recordArea.SetActive(false);
@@ -25,8 +40,11 @@ public class UIButtonManager : MonoBehaviour
 
     public void RecordButton()
     {
-        _isSelect = false;
-        _isRecord = true;
+        _selectToggle.GetComponent<Image>().sprite = null;
+        _selectToggle.GetComponent<Image>().color = _defaultColor;
+
+        _recordToggle.GetComponent<Image>().sprite = _highlight;
+        _recordToggle.GetComponent<Image>().color = _highlightColor;
 
         _selectArea.SetActive(false);
         _recordArea.SetActive(true);
